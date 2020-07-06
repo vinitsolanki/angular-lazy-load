c82fcd488994c0f883911a556cd10027c8b01ede
Chintan Gohel <gohel962@gmail.com>
2020-07-06 20:26:54 +0530

    Updated project to sass to scss and added ag-grid lib

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { AdminComponent } from './layout/admin.component';
 import { AdminRoutingModule, declarations } from './admin-routing.module';
-import { SharedModule } from '../shared/shared.module';
 import { AdminSideMenuComponent } from './layout/side-menu/side-menu.component';
 import { AdminHeaderComponent } from './layout/header/header.component';
 import { AdminFooterComponent } from './layout/footer/footer.component';
-import { LoginComponent } from '../pages/login/login.component';
-import { AddProductComponent } from '../pages/product/add-product/add-product.component';
+import { AgGridModule } from 'ag-grid-angular';
+
 
 @NgModule({
   declarations: [
@@ -17,7 +16,8 @@ import { AddProductComponent } from '../pages/product/add-product/add-product.co
     ...declarations
   ],
   imports: [
-    AdminRoutingModule
+    AdminRoutingModule,
+    AgGridModule.withComponents([])
   ]
 })
 export class AdminModule {
